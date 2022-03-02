201ffd4dc2a94edf0e1f1960da0c349f1b42416a
Seoranglee <22seorang@gmail.com>
2022-03-02 10:27:12 +0900

    update

--- next-todo/pages/index.tsx
+++ next-todo/pages/index.tsx
@@ -1,5 +1,10 @@
 import {NextPage} from "next";
+import styled from "styled-components";
+import React from "react";
 
+const Container = styled.div`
+  font-style:italic;
+`
 
 const index:NextPage = () =>{
   return <div>Hello Typescript</div>
